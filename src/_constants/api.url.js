cc75616ce66cc0c44adf26c87fa936d267b34601
SherLzp <xiaobai084@163.com>
2019-08-20 13:59:02 +0800

    [Sher] create _constants directory and define companyService

---
+++ src/_constants/api.url.js
@@ -0,0 +1,7 @@
+const server = 'http://localhost:8080'
+export const apiUrl = {
+    URL_COMPANY: server + '/company',
+    URL_GOVERNMENT: server + '/government',
+    URL_COMMON: server + '/server',
+    URL_USER: server + '/user'
+}
---
+++ src/_services/company.services.js
@@ -0,0 +1,48 @@
+
+export const companyService = {
+    listBlacklist,
+    listBlacklistByWeid,
+    listLoanRequestRecords,
+    verifyCredential,
+    verifyUserAuthenticity,
+    verifyMultiParityLoan,
+    listLoanRecords,
+    handleLoanRequest,
+    addToBlacklist
+}
+
+function listBlacklist() {
+
+}
+
+function listBlacklistByWeid(weid) {
+
+}
+
+function listLoanRequestRecords(companyName) {
+
+}
+
+function verifyCredential(weid, issuer, type) {
+
+}
+
+function verifyUserAuthenticity(weid) {
+
+}
+
+function verifyMultiParityLoan() {
+
+}
+
+function listLoanRecords(companyName) {
+
+}
+
+function handleLoanRequest(id, handleType) {
+
+}
+
+function addToBlacklist(id) {
+
+}
